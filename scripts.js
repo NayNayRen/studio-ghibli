41f2d69f5f11f8255e35d7777b01e0545b39ddca
Nate M <jeditendencies35@gmail.com>
2022-09-03 23:15:23 -0400

    finally doing updates to ghibli

--- scripts.js
+++ scripts.js
@@ -1,30 +1,36 @@
-
-const submitForm = document.getElementById('search-form');
-const displayAnimeTitle = document.getElementById('display-anime-title');
-const anime = document.getElementById('anime-movies');
+const submitForm = document.querySelector('#search-form');
+const displayAnimeTitle = document.querySelector('#display-anime-title');
+const anime = document.querySelector('#anime-movies');
 const displayAnimeInfo = document.querySelector('.display-anime-info');
-const closeButton = document.getElementById('close-button');
+const closeButton = document.querySelector('#close-button');
 const ghibliHistory = document.querySelector('.ghibli-history');
 
-async function getAnimeTitle(){
+async function getAnimeTitle() {
   const res = await fetch(`https://ghibliapi.herokuapp.com/films`);
   const data = await res.json();
   return data;
 }
-async function showAnimeTitle(e){// shows the title for each film
+async function showAnimeTitle(e) {
+  // shows the title for each film
   e.preventDefault();
   let films = await getAnimeTitle();
-  anime.innerHTML = films.map(film => `
+  anime.innerHTML = films
+    .map(
+      (film) => `
     <a class='film-title' id='film-title'>
     ${film.title}
     </a><br>
-    `).sort().join('');// joins each film to list of films for display
+    `
+    )
+    .sort()
+    .join(''); // joins each film to list of films for display
   displayAnimeTitle.style.display = 'block';
-  closeButton.style.display = 'block';// removes close button
-  ghibliHistory.style.display = 'none';// removes the history window
+  closeButton.style.display = 'block'; // removes close button
+  ghibliHistory.style.display = 'none'; // removes the history window
 }
 
-async function getAnimeByID(filmID){// gets and adds film info to be displayed
+async function getAnimeByID(filmID) {
+  // gets and adds film info to be displayed
   let res = await fetch(`https://ghibliapi.herokuapp.com/films/${filmID}`);
   let data = await res.json();
   const filmTitle = data.title;
@@ -33,7 +39,7 @@ async function getAnimeByID(filmID){// gets and adds film info to be displayed
   const filmDescription = data.description;
   const releaseDate = data.release_date;
   const rating = data.rt_score;
-  let titleWithoutApostrophe = data.title.replace("'", "");
+  let titleWithoutApostrophe = data.title.replace("'", '');
   const span = document.createElement('span');
   span.innerHTML = `
   <div class='display-anime-info-header'>
@@ -46,21 +52,22 @@ async function getAnimeByID(filmID){// gets and adds film info to be displayed
   <span class='film-info'>Rating: ${rating} / 100</span>
   <img class='film-art' src='img/${titleWithoutApostrophe}.jpg'>
   `;
-  displayAnimeInfo.appendChild(span);// adds elements with info to be displayed
+  displayAnimeInfo.appendChild(span); // adds elements with info to be displayed
 }
 
-async function getAnimeInfo(){
+async function getAnimeInfo() {
   const res = await fetch(`https://ghibliapi.herokuapp.com/films`);
   const data = await res.json();
   return data;
 }
 
-async function showAnimeInfo(e){// adds each films info to the DOM when title is clicked
-  if(e.target.id == 'film-title'){
+async function showAnimeInfo(e) {
+  // adds each films info to the DOM when title is clicked
+  if (e.target.id == 'film-title') {
     let title = e.target.text.trim();
     let film = await getAnimeInfo();
-    film.map(filmInfo => {
-      if(title == filmInfo.title){
+    film.map((filmInfo) => {
+      if (title == filmInfo.title) {
         getAnimeByID(filmInfo.id);
         displayAnimeInfo.style.display = 'flex';
       }
@@ -69,7 +76,8 @@ async function showAnimeInfo(e){// adds each films info to the DOM when title is
   }
 }
 
-function closeDisplay(){// closes all info windows and shows history
+function closeDisplay() {
+  // closes all info windows and shows history
   displayAnimeTitle.style.display = 'none';
   displayAnimeInfo.style.display = 'none';
   displayAnimeInfo.innerHTML = '';
